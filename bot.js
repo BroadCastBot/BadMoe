dd73f3c6a9e293434bef288632f616d1ca0b9ede
BroadCastBot <50641382+BroadCastBot@users.noreply.github.com>
2019-09-15 01:31:01 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -42,16 +42,4 @@ client.on('message', async (message) => {
 });
 
 
-client.on('ready', () => {
-   console.log(`----------------`);
-      console.log(`Desert Bot- Script By : i1Suhaib`);
-        console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
-    console.log(`----------------`);
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Bad♡Moe`,"http://twitch.tv/S-F")
-client.user.setStatus("dnd")
-});
-
-
 client.login(process.env.BOT_TOKEN);
